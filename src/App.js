8966f54fa6ab96fad3e1fef84ec3ed05dbad557b
Vwore <vanshaj.wore@gmail.com>
2025-04-11 13:15:04 +0530

    project v6

--- src/App.js
+++ src/App.js
@@ -30,101 +30,106 @@ function App() {
     dob: "",
   });
   return (
-    <div className="App">
-      <h1>User Details Modal</h1>
-      <button
-        onClick={() => {
-          setShowModal(true);
-        }}
-      >
-        Open Form
-      </button>
+    <div id="modal-root">
+      <div className="App">
+        <h1>User Details Modal</h1>
+        <button
+          onClick={() => {
+            setShowModal(true);
+          }}
+        >
+          Open Form
+        </button>
 
-      <Modal
-        isOpen={showModal}
-        onRequestClose={() => {
-          setShowModal(false);
-        }}
-        style={customStyles}
-        contentLabel="Example Modal"
-        shouldCloseOnOverlayClick={true} // ✅ This is needed
-        parentSelector={() => document.querySelector("#modal-root")}
-      >
-        <div className="modal">
-          <div className="modal-content">
-            <h2>Fill Details</h2>
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                if (userInput.phone.length !== 10) {
-                  alert("Invalid phone number");
-                } else if (userInput.email.includes("@") === false) {
-                  alert("Invalid email");
-                } else if (isFutureDate(userInput.dob)) {
-                  alert("Invalid date of birth");
-                } else setShowModal(false);
-              }}
-            >
-              <div>
-                <div>Username:</div>
-                <input
-                  required
-                  id="username"
-                  value={userInput.user}
-                  onChange={(e) => {
-                    setUserInput((prev) => ({ ...prev, user: e.target.value }));
-                  }}
-                ></input>
-              </div>
-              <div>
-                <div>Email Address:</div>
-                <input
-                  required
-                  id="email"
-                  value={userInput.email}
-                  onChange={(e) => {
-                    setUserInput((prev) => ({
-                      ...prev,
-                      email: e.target.value,
-                    }));
-                  }}
-                ></input>
-              </div>
-              <div>
-                <div>Phone Number:</div>
-                <input
-                  required
-                  id="phone"
-                  value={userInput.phone}
-                  onChange={(e) => {
-                    setUserInput((prev) => ({
-                      ...prev,
-                      phone: e.target.value,
-                    }));
-                  }}
-                ></input>
-              </div>
-              <div>
-                <div>Date of Birth:</div>
-                <input
-                  required
-                  id="dob"
-                  value={userInput.dob}
-                  onChange={(e) => {
-                    setUserInput((prev) => ({
-                      ...prev,
-                      dob: e.target.value,
-                    }));
-                  }}
-                ></input>
-              </div>
-              <button className="submit-button" type="submit">
-                Submit
-              </button>
-            </form>
+        <Modal
+          isOpen={showModal}
+          onRequestClose={() => {
+            setShowModal(false);
+          }}
+          style={customStyles}
+          contentLabel="Example Modal"
+          shouldCloseOnOverlayClick={true} // ✅ This is needed
+          parentSelector={() => document.querySelector("#modal-root")}
+        >
+          <div className="modal">
+            <div className="modal-content">
+              <h2>Fill Details</h2>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  if (userInput.phone.length !== 10) {
+                    alert("Invalid phone number");
+                  } else if (userInput.email.includes("@") === false) {
+                    alert("Invalid email");
+                  } else if (isFutureDate(userInput.dob)) {
+                    alert("Invalid date of birth");
+                  } else setShowModal(false);
+                }}
+              >
+                <div>
+                  <div>Username:</div>
+                  <input
+                    required
+                    id="username"
+                    value={userInput.user}
+                    onChange={(e) => {
+                      setUserInput((prev) => ({
+                        ...prev,
+                        user: e.target.value,
+                      }));
+                    }}
+                  ></input>
+                </div>
+                <div>
+                  <div>Email Address:</div>
+                  <input
+                    required
+                    id="email"
+                    value={userInput.email}
+                    onChange={(e) => {
+                      setUserInput((prev) => ({
+                        ...prev,
+                        email: e.target.value,
+                      }));
+                    }}
+                  ></input>
+                </div>
+                <div>
+                  <div>Phone Number:</div>
+                  <input
+                    required
+                    id="phone"
+                    value={userInput.phone}
+                    onChange={(e) => {
+                      setUserInput((prev) => ({
+                        ...prev,
+                        phone: e.target.value,
+                      }));
+                    }}
+                  ></input>
+                </div>
+                <div>
+                  <div>Date of Birth:</div>
+                  <input
+                    required
+                    id="dob"
+                    value={userInput.dob}
+                    onChange={(e) => {
+                      setUserInput((prev) => ({
+                        ...prev,
+                        dob: e.target.value,
+                      }));
+                    }}
+                  ></input>
+                </div>
+                <button className="submit-button" type="submit">
+                  Submit
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
-      </Modal>
+        </Modal>
+      </div>
     </div>
   );
 }
